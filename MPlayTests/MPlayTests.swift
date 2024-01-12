d1295cff4805b31dba5bf675274f0ecd9315b551
Samson Lopez <44753166+samsonlopez@users.noreply.github.com>
2024-01-12 12:22:33 +0000

    Update MPlayTests.swift with description of location for unit tests in the Home module.

--- MPlayTests/MPlayTests.swift
+++ MPlayTests/MPlayTests.swift
@@ -7,3 +7,4 @@
 
 // MARK: Modular app using SPM. Unit Tests included in each module.
 // (Home, Landing, etc)
+// See Home/Tests/HomeTests/HomeViewModelTests.swift with a limited set of tests for the Home module.
